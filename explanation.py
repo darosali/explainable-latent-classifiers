4feac228bae68d8ffcd6627ef08e74005a077721
darosali <darosali@fel.cvut.cz>
2025-03-14 09:37:06 +0100

    add example

--- explanation.py
+++ explanation.py
@@ -11,14 +11,34 @@ def find_knn(x, X_train_n, y_train_n, X_train_a, y_train_a, k=9):
     knn_normal = NearestNeighbors(n_neighbors=k, algorithm='auto', metric='euclidean')
     knn_normal.fit(X_train_n)
     distances_n, indices_n = knn_normal.kneighbors(x, n_neighbors=k)
-    print(indices_n)
-    print(distances_n)
+    #print(indices_n)
+    #print(distances_n)
     
     knn_anomaly = NearestNeighbors(n_neighbors=k, algorithm='auto', metric='euclidean')
     knn_anomaly.fit(X_train_a)
     distances_a, indices_a = knn_anomaly.kneighbors(x, n_neighbors=k)
-    print(indices_a)
-    print(distances_a)
+    #print(indices_a)
+    #print(distances_a)
+
+    lasso_x_train = np.vstack((X_train_n[indices_n.flatten().tolist()], X_train_a[indices_a.flatten().tolist()]))
+    lasso_y_train = np.concatenate((y_train_n[indices_n.flatten().tolist()], y_train_a[indices_a.flatten().tolist()]))
+    print(lasso_x_train.shape)
+    print(lasso_y_train.shape)
+
+    return lasso_x_train, lasso_y_train
+
+def find_knn_latent(x, X_train_n, z_train_n, y_train_n, X_train_a, z_train_a, y_train_a, k=9):
+    knn_normal = NearestNeighbors(n_neighbors=k, algorithm='auto', metric='euclidean')
+    knn_normal.fit(z_train_n)
+    distances_n, indices_n = knn_normal.kneighbors(x, n_neighbors=k)
+    #print(indices_n)
+    #print(distances_n)
+    
+    knn_anomaly = NearestNeighbors(n_neighbors=k, algorithm='auto', metric='euclidean')
+    knn_anomaly.fit(z_train_a)
+    distances_a, indices_a = knn_anomaly.kneighbors(x, n_neighbors=k)
+    #print(indices_a)
+    #print(distances_a)
 
     lasso_x_train = np.vstack((X_train_n[indices_n.flatten().tolist()], X_train_a[indices_a.flatten().tolist()]))
     lasso_y_train = np.concatenate((y_train_n[indices_n.flatten().tolist()], y_train_a[indices_a.flatten().tolist()]))
@@ -31,6 +51,7 @@ def show_importance(coefficients, feature_names, n_features=10):
     feature_importance = np.abs(coefficients)
     sorted_importance = np.argsort(feature_importance)[::-1]
     print(sorted_importance[:n_features])
+    print(f"num of non-zero features: {feature_importance[feature_importance != 0].shape[0]}")
     
     for imp_idx in sorted_importance[:n_features]:
         print(f"{feature_names[imp_idx]}: {coefficients[imp_idx]}")
